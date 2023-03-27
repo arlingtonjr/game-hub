17e5eb67cc15d4d5dff793f693dc0b78d64a6037
aperlota <aperlota@pandai.com>
2023-03-27 13:13:52 +0800

    Show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,44 +2,49 @@ import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
-export interface Platform{
+export interface Platform {
   id: number;
   name: string;
   slug: string;
 }
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: { platform: Platform}[];
-    metacritic: number;
-  }
-
-  interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-  }
-
-const useGames = () =>{
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setErrors] = useState("");
-  
-    useEffect(() => {
-
-        const controller = new AbortController();
-      apiClient
-        .get<FetchGamesResponse>("/games",{signal: controller.signal})
-        .then((res) => setGames(res.data.results))
-        .catch((err) => {
-            if(err instanceof CanceledError) return;
-            setErrors(err.message)
-        } );
-
-        return () => controller.abort();
-
-    }, []);
-
-    return {games, error};
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setErrors] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setErrors(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error, isLoading };
+};
 
 export default useGames;
